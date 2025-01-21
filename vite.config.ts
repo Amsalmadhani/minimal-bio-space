4495714fdb6d48a06a02f9939f2439e8ec6fa02b
Amsalmadhani <43183907+Amsalmadhani@users.noreply.github.com>
2025-01-20 19:27:18 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+base: mode === 'production' ? '/minimal-bio-space/' : '/', // Correct base for GitHub Pages
   server: {
     host: "::",
     port: 8080,
